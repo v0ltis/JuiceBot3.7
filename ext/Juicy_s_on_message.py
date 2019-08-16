2da52ad70339b091aa29240ec15e83dc6bdebe8b
TheLiche-Seregon <46620722+TheLiche-Seregon@users.noreply.github.com>
2019-08-16 21:27:20 +0200

    Update Juicy_s_on_message.py

--- ext/Juicy_s_on_message.py
+++ ext/Juicy_s_on_message.py
@@ -15,7 +15,7 @@ class Messaging(commands.Cog):
 		lang_test_results = await self.bot.what_language(message)
 		message_to_send = ''
 		
-		if message.channel != discord.DMChannel:
+		if type(message.channel) != discord.DMChannel:
 			async def filter(message):
 				try:
 					await message.delete()
