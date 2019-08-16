43ff8548e71e77bbe4421c42d255fffec10ca891
TheLichIsBack <liche.tuto@gmail.com>
2019-08-16 16:23:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -99,6 +99,6 @@ class Juicy(commands.Bot):
 			return (0,0)
 
 Bot = Juicy(command_prefix=Consts.commands_prefix,help_command=None)
-Bot.run('TOKEN_HERE')
+Bot.run(os.environ["TOKEN_HERE"])
 
 print('Finished')
