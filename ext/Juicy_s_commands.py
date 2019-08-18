8e03b7add1dfd8bdba6efa4f028d930abce5621a
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-08-18 15:28:22 +0200

    Update Juicy_s_commands.py

--- ext/Juicy_s_commands.py
+++ ext/Juicy_s_commands.py
@@ -142,14 +142,14 @@ class Commanding(commands.Cog):
 	
 	
 	
-	@commands.command()
-	async def calc(self,ctx):
-		lang_tests_results = await self.bot.what_language(ctx)
-    		mess = Trad.calc[lang_tests_results[1]]
-		cal = await ctx.send("Calcul en cours . . .",delete_after=10)
-		ctx.channel.typing():
-		time.sleep(5)
-		await cal.edit(content=mess)
+	#@commands.command()
+	#async def calc(self,ctx):
+	#	lang_tests_results = await self.bot.what_language(ctx)
+    	#	mess = Trad.calc[lang_tests_results[1]]
+	#	cal = await ctx.send("Calcul en cours . . .",delete_after=10)
+	#	ctx.channel.typing():
+	#	time.sleep(5)
+	#	await cal.edit(content=mess)
 	
 	
 	
