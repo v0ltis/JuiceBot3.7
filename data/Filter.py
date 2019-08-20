445397100ce0fae844a44818e902737beb90b1de
v0ltis <40314287+v0ltis@users.noreply.github.com>
2019-08-20 13:47:34 +0200

    Update Filter.py

--- data/Filter.py
+++ data/Filter.py
@@ -1,10 +1,10 @@
 fitler_FR_in = ['va te faire','va te faire voir','va te faire foutre','va te faire enculé','enculé','enculer'
 'tg','ta mère','nique','bite',"pute","salope","connard","cul","abrutit","encule","chatte","bitch",'bâtard',
-"biatch",'branleur','couilles','chargasse','con ',' con','conasse','connarde',"ducon",'enfoiré','fdp','ta gueule',
+"biatch",'branleur','couilles','chargasse',' con ',' con','conasse','connarde',"ducon",'enfoiré','fdp','ta gueule',
 'ta guele','ta geule','garce',"ta race",'fumier','gland','kikou','noob','tapette','mauviette','merdeux',
 'michto','minus','moins-que-rien','moins que rien','mollusque','naze','sa mère','sac à merde','pd',
 "pédé","plouc",'salop','zguègue','trou de cul',"trou d'uc",'trou du cul',"trou de bal","trouduc",
-'tafiole','tarlouze','ptn','fk ','fuck',' fk']
+'tafiole','tarlouze','ptn',' fk ','fuck',' fk']
 
 fitler_FR_emoji = ['🖕',':middle_finger:']
 fitler_FR_not = ['tapette à ','connerie','hold the fuck up','content']
