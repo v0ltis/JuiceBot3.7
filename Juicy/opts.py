99a37be5bc78257360a5119a19f7122adbeed7cf
TheLichIsBack <liche.tuto@gmail.com>
2020-02-01 12:30:27 +0100

    Update opts.py

--- Juicy/opts.py
+++ Juicy/opts.py
@@ -5,43 +5,30 @@ import json,os
 #
 
 def del_opt(serv, option, new):
-	param = open(str(serv) + "/option.txt","r")
-	param = param.read()
-	param = (json.loads(param) )
+    param = open(str(serv) + "/option.txt","r")
+    param = param.read()
+    param = (json.loads(param) )
 
-	if option == "bw":
-		bw = open(str(serv) + "/bw.txt","r")
-		bw = bw.read()
-		new = new.replace(" ","_")
-		new += " "
-		nFile = bw.replace(new,"")
-		bw = open(str(serv) + "/bw.txt","w")
-		bw.write(nFile)
-		del_opt.good = True
+    #del opt
+    if not new in param[option]:
+        del_opt.already = "oui"
 
-	#del opt
-	elif not new in param[option]:
-		del_opt.already = "oui"
+    else:
+        del_opt.already = None
+        try:
+            param[option] = str(param[option]).replace(" " + str(new),"")
+            del_opt.err = "non"
 
+        except:
+            del_opt.exce = Exception
+            del_opt.err = True
 
-	else:
-		del_opt.already = None
-		try:
+        del_opt.new = param[option]
 
+        new_param = open(str(serv) + "/option.txt","w")
 
-			param[option] = str(param[option]).replace(" " + str(new),"")
-			del_opt.err = "non"
-
-		except:
-			del_opt.exce = Exception
-			del_opt.err = True
-
-		del_opt.new = param[option]
-
-		new_param = open(str(serv) + "/option.txt","w")
-
-		param = json.dumps(param)
-		new_param.write(param)
+        param = json.dumps(param)
+        new_param.write(param)
 
 
 
@@ -49,100 +36,74 @@ def del_opt(serv, option, new):
 #DEF add_opt
 #
 def add_opt(serv, option, new):
-	param = open(str(serv) + "/option.txt","r")
-	param = param.read()
-	param = (json.loads(param) )
+    param = open(str(serv) + "/option.txt","r")
+    param = param.read()
+    param = (json.loads(param) )
 
-	#add opt
-	if option == "bw":
-		bw = open(str(serv) + "/bw.txt","a")
-		new = new.replace(" ","_")
-		new += " "
-		bw.write(new)
-		add_opt.good = True
+    #add opt
+    if new in param[option]:
+        add_opt.already = True
 
+    else:
+        param[option] = param[option] + (" " + str(new))
 
-	elif new in param[option]:
-		add_opt.already = True
+        add_opt.new = param[option]
 
+        new_param = open(str(serv) + "/option.txt","w")
 
-	else:
-		param[option] = param[option] + (" " + str(new))
-
-		add_opt.new = param[option]
-
-		new_param = open(str(serv) + "/option.txt","w")
-
-		param = json.dumps(param)
-		new_param.write(param)
+        param = json.dumps(param)
+        new_param.write(param)
 
 #
 # DEF set_opt
 #
 def set_opt(serv, option, new):
+    param = open(str(serv) + "/option.txt","r")
+    param = param.read()
+    param = (json.loads(param) )
 
+    #new opt :
 
-	if option == "bw":
-		file = open(str(serv) + "/bw.txt","w")
-		file.write("")
+    param[option] = new
 
+    set_opt.new = param[option]
 
-	else:
-		param = open(str(serv) + "/option.txt","r")
-		param = param.read()
-		param = (json.loads(param) )
-
-		#new opt :
-
-		param[option] = new
-
-		set_opt.new = param[option]
-
-		new_param = open(str(serv) + "/option.txt","w")
-		param = json.dumps(param)
-		new_param.write(param)
+    new_param = open(str(serv) + "/option.txt","w")
+    param = json.dumps(param)
+    new_param.write(param)
 
 
 #
 #DEF opt
 #
 def opt(guild,owner):
-	
-	try:
-		serv =  guild.id
+    try:
+        serv =  guild.id
+#       ouvre les paramètres
+        param = open(str(serv) + "/option.txt","r")
+        param = param.read()
+        param = (json.loads(param) )
 
-	#ouvre les paramètres
-		param = open(str(serv) + "/option.txt","r")
-		ban_w = open(str(serv) + "/bw.txt","r")
-		param = param.read()
-		ban_w = ban_w.read()
-		param = (json.loads(param) )
+    except FileNotFoundError:
+#       crée le fichier si il n'existe pas
+        os.makedirs(str(serv))
+        param = open(str(serv) + "/option.txt","a")
+        param.write('{"langue":"en","admin":"' + owner + '","prefix":"/"}')
+#          ouvre les paramètres
+        param = open(str(serv) + "/option.txt","r")
+        param = param.read()
+        param = (json.loads(param) )
 
-	except FileNotFoundError:
-		#crée le fichier si il n'existe pas
-		os.makedirs(str(serv))
-		param = open(str(serv) + "/option.txt","a")
-		param.write('{"langue":"en","admin":"' + owner + '","prefix":"/"}')
-		ban_w = open(str(serv) + "/bw.txt","a")
-		#ouvre les paramètres
-		param = open(str(serv) + "/option.txt","r")
-		ban_w = open(str(serv) + "/bw.txt","a")
-		param = param.read()
-		ban_w = ban_w.read()
-		param = (json.loads(param) )
 
-	ban_w = ban_w.split()
-	opt.bw = [w.replace('_', ' ') for w in ban_w]
+    opt.admin = str(param["admin"])
 
-	opt.admin = str(param["admin"])
+    opt.prefix = str(param["prefix"])
 
-	opt.prefix = str(param["prefix"])
+    opt.langue = str(param["langue"])
 
-	opt.langue = str(param["langue"])
+    try:
+        prem = open(str(serv) + "/premuim.txt","r")
+        opt.premuim = prem.read()
 
-	try:
-		prem = open(str(serv) + "/premuim.txt","r")
-		opt.premuim = prem.read()
-
-	except:
-		opt.premuim = False
+    except:
+        opt.premuim = False
